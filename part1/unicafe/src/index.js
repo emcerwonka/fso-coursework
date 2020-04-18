4fa1e4c8918e0e00df421d9cd4d4abe6dc27275a
emcerwonka <evancerwonka@gmail.com>
2020-04-18 17:51:29 -0500

    Completed exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom'
 
 const Section = ({ text }) => <h1>{text}</h1>
 
-const Stats = ({ good, neutral, bad }) => {
+const Stats = ({ good, neutral, bad, all, avg, pos }) => {
   return (
     <div>
       <p>g oo d : {good}</p>
       <p>n eu t ra l : {neutral}</p>
       <p>b ad : {bad}</p>
+      <p>a ll : {all}</p>
+      <p>a ve r ag e : {avg}</p>
+      <p>p os i ti v e : {pos}</p>
     </div>
   )
 }
 
 const Button = ({ text, handler }) => {
-  console.log('text =', text);
   return <button onClick={handler}>{text}</button>
 }
 
@@ -23,6 +25,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  let all = good + neutral + bad
+  let avg = (all !== 0) ? ((good * 1) + (neutral * 0) + (bad * -1) / all) : 0
+  let pos = (all !== 0) ? (good / all) : 0
 
   return (
     <div>
@@ -31,7 +37,14 @@ const App = () => {
       <Button text='N EU T RA L' handler={() => setNeutral(neutral + 1)} />
       <Button text='B AD' handler={() => setBad(bad + 1)} />
       <Section text='* S T A T I S T I C S *' />
-      <Stats good={good} neutral={neutral} bad={bad} />
+      <Stats 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        avg={avg}
+        pos={pos}
+      />
     </div>
   )
 }

84c6d5e6a88d439d72fafc1d0def092a98450d22
emcerwonka <evancerwonka@gmail.com>
2020-04-22 16:57:29 -0500

    Completed exercise 2.5

--- part2/courseinfo2/src/index.js
+++ part2/courseinfo2/src/index.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-
-}
-
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) =>
-        <Part key={part.name} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, part) => total + part.exercises, 0)
-  return (
-    <p>Number of exercises : {sum}</p>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [

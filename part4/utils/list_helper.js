b9cb54307f44bff57da6395e434b0763103851bd
emcerwonka <evancerwonka@gmail.com>
2020-05-16 16:39:39 -0500

    Completed exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
-  return 1;
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const likeSum = (sumLikes, nextLike) => {
+    return sumLikes + nextLike
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.map(blog => blog.likes).reduce(likeSum)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }

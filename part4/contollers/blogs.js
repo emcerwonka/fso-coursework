9696ddea86a3533f3981fcda56eedd205683c78c
emcerwonka <evancerwonka@gmail.com>
2020-07-06 19:18:48 -0500

    Updated blogs controller.

--- part4/contollers/blogs.js
+++ part4/contollers/blogs.js
@@ -10,21 +10,8 @@ blogsRouter.get('/', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-<<<<<<< HEAD
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog.toJSON())
-=======
-  blog
-    .save()
-    .then(blog => {
-      if (blog) {
-        response.status(201).json(blog.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
->>>>>>> e2bdb78056a9eb82a07e2d188d9e00768290737f
 })
 
 module.exports = blogsRouter

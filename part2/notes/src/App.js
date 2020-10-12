ee431a95435ba7f33baab23cd2875a011f03ee58
emcerwonka <evancerwonka@gmail.com>
2020-10-12 17:51:44 -0500

    Updated notes frontend to allow logins.

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
+import loginService from './services/login'
 import noteService from './services/notes'
 import Footer from './components/Footer'
 
@@ -9,6 +10,9 @@ const App = (props) => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -17,6 +21,49 @@ const App = (props) => {
         setNotes(initialNotes)
       })
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">L O G I N</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">S A V E</button>
+    </form>
+  )
 
   const notesToShow = showAll
     ? notes
@@ -37,6 +84,32 @@ const App = (props) => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+    console.log('Logging in with ', username, password)
   }
 
   const handleNoteChange = (event) => {
@@ -70,7 +143,19 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification message={errorMessage} />
+
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in.</p>
+          {noteForm()}
+        </div>
+      }
+
+      <h2>Notes</h2>
+
       <div>
         <button onClick={toggleShowAll}>
           SHOW {showAll ? 'IMPORTANT' : 'ALL'}
@@ -85,13 +170,7 @@ const App = (props) => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">S A V E</button>
-      </form>
+
       <Footer />
     </div>
   )
